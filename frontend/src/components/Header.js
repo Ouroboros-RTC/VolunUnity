5404604abffc0bd0e334f8b6e149526d98b29d76
kehanxiao <xiao.keh@northeastern.edu>
2024-02-03 23:21:34 -0800

    modify style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,11 @@ const Header = () => {
     <header>
       <div className="container">
         <Link to="/">
+          <img src="logo.png" alt="Logo" />
           <h1>Volunteer Unity</h1>
+          <h3>
+            Uniting <span className="highlight-hearts">Hearts</span>, Transforming <span className="highlight-lives">Lives</span>
+          </h3>
         </Link>
       </div>
     </header>
